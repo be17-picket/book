248724470f6bc36177f8a5db72c6b2b18b598007
wonzzu <wonzzu@naver.com>
2025-08-07 11:26:48 +0900

    feat:reviewDto

--- src/main/java/org/example/spring/review/ReviewDto.java
+++ src/main/java/org/example/spring/review/ReviewDto.java
@@ -8,7 +8,19 @@ public class ReviewDto {
     @Getter
     @Builder
     public class reviewRes{
-        private Integer idx
+        private Integer idx;
+        private String  content1;
+        private Integer reviewScore1;
+
+        public static ReviewDto.reviewRes from(Review entity) {
+            ReviewDto.reviewRes dto  = reviewRes.builder()
+                    .idx(entity.getIdx())
+                    .content1(entity.getContent())
+                    .reviewScore1(entity.getReviewScore())
+                    .build();
+
+            return dto;
+        }
 
     }
 }
